381b75cf4c2982363207a8a283c0301b9fc82f0d
Colin <colin.combe@ed.ac.uk>
2018-07-19 12:00:33 +0100

    Update xiSpecConfig.php

--- xiSpecConfig.php
+++ xiSpecConfig.php
@@ -1,4 +1,4 @@
 <?php
 echo "CLMSUI.xiSpecBaseDir = \"/xiView_container/spectrum/\";\n"; // you may need to edit this depending on where xiView_container is in web server folder
-echo "CLMSUI.xiAnnotRoot = \"http://URL of your xiAnnotator installatoin/xiAnnotator/\";\n";          
+echo "CLMSUI.xiAnnotRoot = \"http://URL of your xiAnnotator service/xiAnnotator/\";\n";          
 ?>
